2c51d09f330ecbc5ea4f11f82790eb86e228df2f
Vladimir Ananiev <vladimir.ananiev@aurea.com>
2017-07-11 18:50:09 +0300

    Created by Coding Ground Client

---
+++ New Project-20170711/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+//   cout << "Hello World" << endl; 
+
+    char name[100];
+    scanf("%s", name);
+    puts(name);
+    
+    return 0;
+}
+
